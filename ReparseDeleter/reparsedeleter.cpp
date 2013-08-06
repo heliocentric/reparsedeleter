aef9071ee4e1db277ea26aedf1a5cdb0e660a16b
Dylan Cochran <heliocentric@gmail.com>
2013-08-06 17:00:46 -0400

    Add some changes.

--- ReparseDeleter/reparsedeleter.cpp
+++ ReparseDeleter/reparsedeleter.cpp
@@ -1,11 +1,14 @@
 #include "reparsedeleter.h"
 #include "ui_reparsedeleter.h"
+#include <Qdir.h>
+
 
 ReparseDeleter::ReparseDeleter(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ReparseDeleter)
 {
     ui->setupUi(this);
+
 }
 
 ReparseDeleter::~ReparseDeleter()
